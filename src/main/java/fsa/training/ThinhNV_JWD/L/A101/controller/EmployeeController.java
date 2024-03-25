17e14630222b99cf30a9c2cb81c54dcf35774c2e
nvtmeta <thinh30102002@gmail.com>
2024-03-25 15:45:27 +0700

    sdfsdf

--- src/main/java/fsa/training/ThinhNV_JWD/L/A101/controller/EmployeeController.java
+++ src/main/java/fsa/training/ThinhNV_JWD/L/A101/controller/EmployeeController.java
@@ -1,22 +1,35 @@
 package fsa.training.ThinhNV_JWD.L.A101.controller;
 
 
+import fsa.training.ThinhNV_JWD.L.A101.model.entity.Employee;
+import fsa.training.ThinhNV_JWD.L.A101.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/employees")
-    public String employee() {
+    public String employee(Model model) {
+        List<Employee> employees = employeeService.getAllEmployees();
+
+        model.addAttribute("employees", employees);
         return "view/employee/list";
     }
 
 
     @GetMapping("/employees/create")
     public String createEmployee() {
-        return "view/employee/create";
+        return "view/employee/add";
     }
 
 
--- src/main/java/fsa/training/ThinhNV_JWD/L/A101/service/impl/EmployeeServiceImpl.java
+++ src/main/java/fsa/training/ThinhNV_JWD/L/A101/service/impl/EmployeeServiceImpl.java
@@ -3,10 +3,12 @@ package fsa.training.ThinhNV_JWD.L.A101.service.impl;
 import fsa.training.ThinhNV_JWD.L.A101.model.entity.Employee;
 import fsa.training.ThinhNV_JWD.L.A101.repository.EmployeeRepository;
 import fsa.training.ThinhNV_JWD.L.A101.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
 
